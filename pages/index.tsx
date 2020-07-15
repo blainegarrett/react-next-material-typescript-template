db9db0443a897fbf3a113623732a53397664145a
Blaine Garrett <blaine@blainegarrett.com>
2020-07-15 16:42:14 -0500

    Getting ESLint installed and setup with TypeScript and VSCode.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,31 @@
-
 import React from 'react';
-import {GetStaticProps, NextPage, GetStaticPropsContext, GetStaticPropsResult} from 'next';
+import {
+  GetStaticProps, NextPage, GetStaticPropsContext, GetStaticPropsResult,
+} from 'next';
 
 interface IndexProps {
   greeting: string
 }
 
 const IndexPage:NextPage<IndexProps> = (props: IndexProps) => {
-    let {greeting} = props;
+  const { greeting } = props;
 
-    return (
-      <div>
-        <h1>{greeting}👋 </h1>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>
+        {greeting}
+        👋
+        {' '}
+      </h1>
+    </div>
+  );
+};
 
-export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext): Promise<GetStaticPropsResult<IndexProps>> => {
-  return {
-    props: {
-      greeting: 'Hello Next.js',
-    },
-  }
-}
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext):
+  Promise<GetStaticPropsResult<IndexProps>> => ({
+  props: {
+    greeting: 'Hello Next.js',
+  },
+});
 
 export default IndexPage;
