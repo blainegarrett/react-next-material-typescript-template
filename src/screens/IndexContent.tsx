f893d11444dc102f343012b080d197f1341b73d8
Blaine Garrett <blaine@blainegarrett.com>
2020-07-20 13:48:50 -0500

    Adding Material-UI

--- src/screens/IndexContent.tsx
+++ src/screens/IndexContent.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import getConfig from 'next/config';
+import Button from '@material-ui/core/Button';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 
 const { publicRuntimeConfig } = getConfig();
 
-interface IndexProps { greeting: string }
+const useStyles = makeStyles((theme: Theme) => ({
+  customButton: {
+    backgroundColor: '#6f2da8',
+    color: theme.palette.common.white,
+    fontFamily: theme.accentFontFamily,
+  },
+}));
 
+interface IndexProps { greeting: string }
 const IndexContent: React.FC<IndexProps> = (props) => {
   const { greeting } = props;
+  const classes = useStyles();
 
   return (
     <div>
@@ -15,6 +25,7 @@ const IndexContent: React.FC<IndexProps> = (props) => {
         {publicRuntimeConfig.greeting_emoji}
         !
       </h1>
+      <Button variant="contained" className={classes.customButton}>A Simple Button</Button>
     </div>
   );
 };
